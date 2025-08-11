0b85fe42f24598c316e6baf78b27f9733f1092d9
Chuthuka <chuthuka097dedunu@gmail.com>
2025-08-11 08:28:42 +0530

    Add Entities

---
+++ src/main/java/com/cpd_hotel_system/auth_service_api/entity/SystemUser.java
@@ -0,0 +1,63 @@
+package com.cpd_hotel_system.auth_service_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name="system_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class SystemUser {
+    @Id
+    @Column(name="user_id",length = 80,nullable = false)
+    private String userId;
+
+    @Column(name="keycloak_id",length = 100,nullable = false)
+    private String keycloakId;
+
+    @Column(name="first_name",length = 80,nullable = false)
+    private String firstName;
+
+    @Column(name="last_name",length = 80,nullable = false)
+    private String lastName;
+
+    @Column(name="email",length = 80,nullable = false)
+    private String email;
+
+    @Column(name="contact",length = 20,nullable = false)
+    private String contact;
+
+    @Column(name="is_active")
+    private boolean isActive;
+
+    @Column(name="is_account_non_locked")
+    private boolean isAccountNonLocked;
+
+    @Column(name="is_credentials_non_expired")
+    private boolean isCredentialsNonExpired;
+
+    @Column(name="is_enabled")
+    private boolean isEnabled;
+
+    @Column(name="is_email_verified")
+    private boolean isEmailVerified;
+
+    @Column(name="is_created_at")
+    private Instant createdAt;
+
+    @Column(name="is_updated_at")
+    private Instant updatedAt;
+
+    @OneToOne(mappedBy = "systemUser")
+    private Otp otp;
+
+
+}
