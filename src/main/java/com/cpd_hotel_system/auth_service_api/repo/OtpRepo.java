55987013e1c3f46699caa1ef9a05d495da43a650
Chuthuka <chuthuka097dedunu@gmail.com>
2025-08-11 08:36:52 +0530

    Add repo classes

---
+++ src/main/java/com/cpd_hotel_system/auth_service_api/repo/OtpRepo.java
@@ -0,0 +1,7 @@
+package com.cpd_hotel_system.auth_service_api.repo;
+
+import com.cpd_hotel_system.auth_service_api.entity.SystemUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OtpRepo extends JpaRepository<SystemUser, String> {
+}
